ae40a0bbd6ebaaa964e135b0d44d062e78095642
rajesh8707 <rajesh2000alld@gmail.com>
2024-10-17 21:30:54 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,68 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [equation, setEquation] = useState("");
+
+  const handleKeyPress = (key) => {
+    setEquation(equation + key);
+  };
+
+  const calculate = () => {
+    try {
+      setEquation(eval(equation));
+    } catch {
+      setEquation("Error");
+    }
+  };
+
+  const handleClear = () => {
+    setEquation(equation.substring(0, equation.length - 1));
+  };
+
+  const handleClearAll = () => {
+    setEquation("");
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="background">
+        <div className="calculator">
+          <input
+            value={equation}
+            onChange={(e) => setEquation(e.target.value)}
+            type="text"
+            className="display"
+          />
+          <div className="keypad">
+            <button className="key clrbtn" onClick={handleClear}>C</button>
+            <button className="key clrbtn" onClick={handleClearAll}>CE</button>
+            <button className="key oprbtn" onClick={() => handleKeyPress("%")}>%</button>
+            <button className="key oprbtn" onClick={() => handleKeyPress("/")}>/</button>
+
+            <button className="key numbtn" onClick={() => handleKeyPress("7")}>7</button>
+            <button className="key numbtn" onClick={() => handleKeyPress("8")}>8</button>
+            <button className="key numbtn" onClick={() => handleKeyPress("9")}>9</button>
+            <button className="key oprbtn" onClick={() => handleKeyPress("*")}>x</button>
+
+            <button className="key numbtn" onClick={() => handleKeyPress("4")}>4</button>
+            <button className="key numbtn" onClick={() => handleKeyPress("5")}>5</button>
+            <button className="key numbtn" onClick={() => handleKeyPress("6")}>6</button>
+            <button className="key oprbtn" onClick={() => handleKeyPress("-")}>-</button>
+
+            <button className="key numbtn" onClick={() => handleKeyPress("1")}>1</button>
+            <button className="key numbtn" onClick={() => handleKeyPress("2")}>2</button>
+            <button className="key numbtn" onClick={() => handleKeyPress("3")}>3</button>
+            <button className="key oprbtn" onClick={() => handleKeyPress("+")}>+</button>
+
+            <button className="key numbtn" onClick={() => handleKeyPress("00")}>00</button>
+            <button className="key numbtn" onClick={() => handleKeyPress("0")}>0</button>
+            <button className="key numbtn" onClick={() => handleKeyPress(".")}>.</button>
+            <button className="key eqlbtn" onClick={calculate}>=</button>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
